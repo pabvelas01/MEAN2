c4cacabbecbf527c4a3f244c9a2ff86519693331
Pablo Velasquez <pvelasquez@ubiobio.cl>
2023-01-10 23:43:56 -0300

    Agrega componente uno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule
